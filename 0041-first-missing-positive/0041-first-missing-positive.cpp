1ad9e0cfbebf124fd2635fc564f977ad0ba40169
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-09 11:16:16 +0530

    Time: 69 ms (83.5%), Space: 41.1 MB (92.58%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -5,7 +5,9 @@ public:
 
         for (int i = 0; i < n; i++) {
             while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
-                swap(nums[i], nums[nums[i] - 1]);
+                int temp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[temp - 1] = temp;
             }
         }
         for (int i = 0; i < n; i++) {
