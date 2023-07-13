c9ad9f018ccf9b958dbb555c7e32b6c958253138
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-13 17:56:12 +0530

    Time: 8 ms (22.23%), Space: 8.3 MB (19.46%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < arr.size(); i++){
+            mp[arr[i]]++;
+        }
+        
+        unordered_set<int> st;
+        
+        for(auto x : mp){
+            st.insert(x.second);
+        }
+        
+        return st.size() == mp.size();
+    }
+};
