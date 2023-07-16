ab26417cbeeb55fae47c1fd14274e66b7dd69199
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-16 14:12:53 +0530

    Time: 3 ms (68.52%), Space: 6.1 MB (24.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<2) return n;
-        else return fib(n-1)+fib(n-2);
+        if (n < 2)
+            return n;
+        
+        int prev = 0;
+        int curr = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        
+        return curr;
     }
 };
