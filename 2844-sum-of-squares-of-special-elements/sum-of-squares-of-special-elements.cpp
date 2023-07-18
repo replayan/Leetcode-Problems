6e48e2a82ae6676ae4705f3b8f42cc31a5099877
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-18 12:02:09 +0530

    Time: 28 ms (6.71%) | Memory: 35.5 MB (8.87%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int sumOfSquares(vector<int>& nums) {
-        long long int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums.size() % (i+1) == 0) {
-                sum += nums[i] * nums[i];
-            }
+    int n = nums.size();
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            sum += nums[i - 1] * nums[i - 1];
         }
-        return sum;
+    }
+    return sum;
     }
 };
