e120bedb06118feaec0248c7c6b78e51ae65214e
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-20 12:32:42 +0530

    Time: 18 ms (47.46%) | Memory: 19.1 MB (81.36%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.php
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param String $s
+     * @param String $p
+     * @return Boolean
+     */
+    function isMatch($s, $p) {
+        if ($s === null || $p === null) {
+            return false;
+        }
+
+        $dp = array_fill(0, strlen($s) + 1, array_fill(0, strlen($p) + 1, false));
+        $dp[0][0] = true;
+
+        for ($i = 0; $i < strlen($p); $i++) {
+            if ($p[$i] === '*' && $dp[0][$i - 1]) {
+                $dp[0][$i + 1] = true;
+            }
+        }
+
+        for ($i = 0; $i < strlen($s); $i++) {
+            for ($j = 0; $j < strlen($p); $j++) {
+                if ($p[$j] === '.') {
+                    $dp[$i + 1][$j + 1] = $dp[$i][$j];
+                }
+                if ($p[$j] === $s[$i]) {
+                    $dp[$i + 1][$j + 1] = $dp[$i][$j];
+                }
+                if ($p[$j] === '*') {
+                    if ($p[$j - 1] !== $s[$i] && $p[$j - 1] !== '.') {
+                        $dp[$i + 1][$j + 1] = $dp[$i + 1][$j - 1];
+                    } else {
+                        $dp[$i + 1][$j + 1] = ($dp[$i + 1][$j] || $dp[$i][$j + 1] || $dp[$i + 1][$j - 1]);
+                    }
+                }
+            }
+        }
+
+        return $dp[strlen($s)][strlen($p)];
+    }
+}
