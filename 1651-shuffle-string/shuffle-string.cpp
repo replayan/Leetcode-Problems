418f1e8d9d6fffdc75d7cb98c63cb6c20060d9f3
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-23 23:49:28 +0530

    Time: 13 ms (34.32%) | Memory: 15 MB (99.30%) - LeetSync

--- 1651-shuffle-string/shuffle-string.cpp
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        for (int i = 0; i < s.size(); i++) {
-            while (indices[i] != i) {
-                swap(s[i], s[indices[i]]);
-                swap(indices[i], indices[indices[i]]);
-            }
+        string ss=s;
+        for(int i=0;i<indices.size();i++){
+            ss[indices[i]]=s[i];
         }
-        return s;
+        return ss;
     }
 };
