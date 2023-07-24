3eac728db8b5867f75760141ad71739d01c76e32
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-25 00:24:21 +0530

    Time: 1 ms (99.82%) | Memory: 49.1 MB (8.09%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,35 @@
-// BRUTE FORCE APPROACH
-// TC: O(N^2)
+// variation of the Boyer-Moore Majority Vote algorithm
+// TC: O(N) SC: O(1)
 
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityCount = nums.length / 2;
+        int majorityElement = 0;
+        int count = 0;
         
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == num) {
-                    count++;
-                }
-            }
-            if (count > majorityCount) {
-                return num;
+            if (count == 0) {
+                majorityElement = num;
+                count = 1;
+            } else if (majorityElement == num) {
+                count++;
+            } else {
+                count--;
             }
         }
-        return -1;
+        
+        int majorityCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == majorityElement) {
+                majorityCount++;
+            }
+        }
+        
+        if (majorityCount > nums.length / 2) {
+            return majorityElement;
+        } else {
+            return -1;
+        }
     }
 }
+
