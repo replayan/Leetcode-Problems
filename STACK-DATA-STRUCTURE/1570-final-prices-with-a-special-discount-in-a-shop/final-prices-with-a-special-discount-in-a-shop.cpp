31f416047b663923f8609597f7a3c93de65bafa4
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-29 16:46:41 +0530

    Time: 2 ms (89.17%) | Memory: 9.9 MB (33.84%) - LeetSync

--- STACK-DATA-STRUCTURE/1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ STACK-DATA-STRUCTURE/1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,15 +3,18 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
         vector<int> result(n);
+        stack<int> stack;
 
         for (int i = 0; i < n; i++) {
-            result[i] = prices[i];
-            for (int j = i + 1; j < n; j++) {
-                if (prices[j] <= prices[i]) {
-                    result[i] = prices[i] - prices[j];
-                    break; 
-                }
+            while (!stack.empty() && prices[i] <= prices[stack.top()]) {
+                result[stack.top()] = prices[stack.top()] - prices[i];
+                stack.pop();
             }
+            stack.push(i);
+        }
+        while (!stack.empty()) {
+            result[stack.top()] = prices[stack.top()];
+            stack.pop();
         }
         return result;
     }
