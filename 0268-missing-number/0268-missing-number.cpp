090d2609961f32e57f4e0e9b1dc3fd2cff42480f
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-07-31 21:31:56 +0530

    Time: 15 ms (93.83%), Space: 18 MB (38.24%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
 
-        for (int i = 0; i < n; i++) {
-            while (nums[i] >= 0 && nums[i] < n && nums[i] != nums[nums[i]]) {
-                swap(nums[i], nums[nums[i]]);
-            }
+        for (int num : nums) {
+            actualSum += num;
         }
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i) {
-                return i;
-            }
-        }
-        return n;
+        
+        return expectedSum - actualSum;
     }
 };
