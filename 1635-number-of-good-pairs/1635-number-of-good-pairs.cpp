7c91a67007d9ce7a7af5939e77a9efbd35b3ef67
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-02 01:22:03 +0530

    Time: 0 ms (100%), Space: 7.2 MB (73.85%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,13 +1,14 @@
+// Hashmap Solution
+
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        unordered_map<int, int> frequency;
-        int goodPairs = 0;
-
-        for (int num : nums) {
-            goodPairs += frequency[num];
-            frequency[num]++;
+        unordered_map<int,int> elementFreq;
+        int goodPairCount = 0;
+        for(int i : nums){
+            goodPairCount += elementFreq[i];
+            elementFreq[i]++;
         }
-        return goodPairs;
+        return goodPairCount;
     }
 };
