12d9e8bb6981fa5d6f73309f710a9a25cba40c27
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-03 11:06:09 +0530

    Time: 3 ms (37.80%) | Memory: 6.1 MB (8.92%) - LeetSync

--- Top-50-Array/1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ Top-50-Array/1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,11 +1,18 @@
+// optimized but takes o(nlogn)
+
 class Solution {
 public:
     int kthFactor(int n, int k) {
         vector<int> res;
-        for(int i=1; i<=n; i++){
-            if(n % i == 0) 
+        for(int i=1; i*i<=n; i++){
+            if(n % i == 0){
                 res.push_back(i);
+                if(i != n/i){
+                    res.push_back(n/i);
+                }
+            }
         }
+        sort(res.begin(), res.end());
         if(k <= res.size()){
             return res[k-1];
         }
