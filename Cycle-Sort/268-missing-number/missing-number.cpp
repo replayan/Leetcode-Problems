3dedd15d459342af43b77731f88abcd5d3d74b1b
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-03 18:04:59 +0530

    Time: 8 ms (99.00%) | Memory: 17.9 MB (83.92%) - LeetSync

---
+++ Cycle-Sort/268-missing-number/missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for (int i = 0; i < nums.size();) {
+            int correct = nums[i];
+            if (nums[i] < nums.size() && nums[i] != nums[correct]) {
+                swap(nums[i], nums[correct]);
+            } else {
+                i++;
+            }
+        }
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
