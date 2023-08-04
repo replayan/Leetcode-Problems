e378f3dcd4673453b485c2f70bbd1f635a226928
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-04 13:47:48 +0530

    Time: 71 ms (99.17%) | Memory: 61.2 MB (40.13%) - LeetSync

--- Cycle-Sort/287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ Cycle-Sort/287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            int correct=nums[i]-1;
-            if(nums[i] != nums[correct]){
-                swap(nums[i],nums[correct]);
-                --i;
-            }
-        }
-        for(int i=0; i<n; i++){
+        int n=nums.size(),i = 0;
+        while(i < n){
             if(nums[i] != i+1){
-                return nums[i];
+                int correct = nums[i]-1;
+                if(nums[i] != nums[correct]){
+                    swap(nums[i],nums[correct]);
+                } else{
+                    return nums[i];
+                }
+            } else {
+                i++;
             }
         }
-        return 1;
+        return -1;
     }
 };
