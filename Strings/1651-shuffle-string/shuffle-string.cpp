7509d8d8028287e93acf20193f3720257094d1ec
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-06 00:41:25 +0530

    Time: 4 ms (89.73%) | Memory: 15.1 MB (50.51%) - LeetSync

---
+++ Strings/1651-shuffle-string/shuffle-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string ss = s;
+        for(int i=0; i<indices.size(); ++i){
+            ss[indices[i]] = s[i];
+        }
+        return ss;
+    }
+};
