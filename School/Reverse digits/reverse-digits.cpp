b922eb1c589b83ed607fb23e4612153fef768aeb
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-07 13:27:07 +0530

    Added Solution - GfG to GitHub

---
+++ School/Reverse digits/reverse-digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int reverse_digit(long long int n)
+		{
+		    // Code here
+		    uint64_t ans=0;
+		    while(n>0){
+		        long long int rem=n % 10;
+		        n /= 10;
+		        ans = ans * 10 + rem;
+		    }
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.reverse_digit(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
