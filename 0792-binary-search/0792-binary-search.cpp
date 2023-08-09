1408e9318d7c3919c02c04e26a3c376e88b7da49
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-09 13:29:49 +0530

    Time: 29 ms (88.17%), Space: 27.6 MB (32.02%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,13 +1,28 @@
-// Practice
+// order agnostic binary search
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int start=0, end=nums.size()-1;
+        bool isAsc=nums[start]<nums[end];
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(target>nums[mid]) start=mid+1;
-            else if(target<nums[mid]) end=mid-1;
-            else return mid;
+            if(nums[mid]==target) return mid;
+
+            if(isAsc){
+                if(target>nums[mid]){
+                    start=mid+1;
+                } 
+                else {
+                    end=mid-1;
+                }
+            }else{
+                if(target<nums[mid]){
+                    start=mid+1;
+                }
+                else {
+                    end=mid-1;
+                }
+            }
         }
         return -1;
     }
