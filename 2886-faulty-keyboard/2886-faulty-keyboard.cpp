d096add91f4c949cc52ce2d5d674b6ddcd90bd97
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-10 10:13:07 +0530

    Time: 6 ms (47.34%), Space: 6.6 MB (41.2%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.cpp
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-    string finalString(const string &s) {
-        deque<char> dq;
-        bool flip = false;
-        
-        for (char ch : s) {
-            if (ch == 'i') {
-                flip = !flip;
-            } else {
-                if (flip) {
-                    dq.push_front(ch);
-                } else {
-                    dq.push_back(ch);
-                }
-            }
+    string finalString(string s) {
+        string ans = "";
+        for(auto c: s){
+            if( c != 'i' ) ans += c;
+            else reverse(ans.begin(), ans.end());
         }
-        string result;
-        for (char ch : dq) {
-            result += ch;
-        }
-        return flip ? string(rbegin(result), rend(result)) : result;
+        return ans;
     }
 };
