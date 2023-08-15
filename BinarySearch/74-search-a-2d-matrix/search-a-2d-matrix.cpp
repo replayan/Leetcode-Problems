ea7f97cec8f823636c94087def78512a902ded72
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-16 00:37:07 +0530

    Time: 7 ms (20.89%) | Memory: 9.5 MB (27.14%) - LeetSync

---
+++ BinarySearch/74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+        
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int left = 0, right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / cols][mid % cols];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
