9f7970a5050de7da6481c63cdbe1dadcdb423cef
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-19 18:23:48 +0530

    Time: 14 ms (83.91%), Space: 16.9 MB (66.12%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,16 +3,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            bool duplicate=false;
-            for(int j=0; j<n; j++){
-                if(i != j && nums[j]==nums[i]){
-                    duplicate=true;
-                    break;
-                }
-            }
-            if(!duplicate){
+    //     int n=nums.size();
+    //     for(int i=0; i<n; i++){
+    //         bool duplicate=false;
+    //         for(int j=0; j<n; j++){
+    //             if(i != j && nums[j]==nums[i]){
+    //                 duplicate=true;
+    //                 break;
+    //             }
+    //         }
+    //         if(!duplicate){
+    //             return nums[i];
+    //         }
+    //     }
+    //     throw invalid_argument("No single number found.");
+    // }
+
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i+=2) {
+            if (i + 1 >= nums.size() || nums[i] != nums[i + 1]) {
                 return nums[i];
             }
         }
