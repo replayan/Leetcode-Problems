edb9a774470ca47e8717c794d66e21ee271a6c0c
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-21 00:09:28 +0530

    Time: 15 ms (95.53%) | Memory: 22.3 MB (78.13%) - LeetSync

--- BinarySearch/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ BinarySearch/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,23 +1,23 @@
-// NAIVE SOLUTION TC: O(N)
-
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
-        if (n==1) return nums[0];
-        for(int i=0; i<nums.size(); i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
-            } else if(i==n-1){
-                if(nums[i]!=nums[i-1]){
-                    return nums[i];
-                }
+
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0]; // eliminating the first element
+        if(nums[n-1]!=nums[n-2]) return nums[n-1]; // eliminating the last element
+
+        int start=1;
+        int end=n-2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            } else if((mid%2 == 1 && nums[mid]==nums[mid-1]) // left part
+            || (mid%2 == 0 && nums[mid]==nums[mid+1])){
+                start=mid+1;
             } else{
-                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                    return nums[i];
-                }
+                end=mid-1; // right part
             }
         }
         return -1;
