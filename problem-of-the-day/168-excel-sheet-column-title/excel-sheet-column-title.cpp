58e20a2c0d98d616b08fe7ea5a504b324f28ec06
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-23 00:37:42 +0530

    Time: 2 ms (49.78%) | Memory: 6 MB (37.93%) - LeetSync

--- problem-of-the-day/168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ problem-of-the-day/168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
+        // Initialize an empty string to store the column title
         string column_title = "";
         
+        // Loop until the column number becomes 0
         while (columnNumber > 0) {
+            // Calculate the remainder when subtracting 1 and then dividing by 26
             int remainder = (columnNumber - 1) % 26;
-            column_title = char('A' + remainder) + column_title;
+
+            // Convert the remainder to a letter ('A' + remainder)
+            char letter = 'A' + remainder;
+
+            // Add the letter to the beginning of the column title
+            column_title = letter + column_title;
+
+            // Reduce the column number for the next iteration
             columnNumber = (columnNumber - 1) / 26;
         }
+        // Return the constructed column title
         return column_title;
     }
 };
