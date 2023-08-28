a4b20d74f88bf8ed13354cc7822e7c29bab11b96
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-08-28 13:11:31 +0530

    Time: 3 ms (97.84%), Space: 10.1 MB (7.55%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            
-            if (count == 1) {
-                return nums[i];
+        unordered_map<int, int> frequency;
+
+        for (int i = 0; i < nums.size(); i++) { // COUNTING THE FREQ OF EACH ELEMENT
+            int num = nums[i];
+            frequency[num]++;
+        }
+
+        for (int i = 0; i < nums.size(); i++) { // CHECKING
+            int num = nums[i];
+            if (frequency[num] == 1) {
+                return num;
             }
         }
         return -1;
