68d1363b050f848b0114b6bda1d28ccab573c561
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-09-02 12:37:24 +0530

    Time: 3 ms (84.00%) | Memory: 8.8 MB (26.34%) - LeetSync

--- Bit-Manipulation/861-flipping-an-image/flipping-an-image.cpp
+++ Bit-Manipulation/861-flipping-an-image/flipping-an-image.cpp
@@ -1,11 +1,31 @@
+// class Solution {
+// public:
+//     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+//         for (vector<int>& row : image) {
+//             for (int i = 0; i < (row.size() + 1) / 2; i++) {
+//                 int temp = row[i] ^ 1;
+//                 row[i] = row[row.size() - i - 1] ^ 1;
+//                 row[row.size() - i - 1] = temp;
+//             }
+//         }
+//         return image;
+//     }
+// };
+// Two pointer :
+
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
         for (vector<int>& row : image) {
-            for (int i = 0; i < (row.size() + 1) / 2; i++) {
-                int temp = row[i] ^ 1;
-                row[i] = row[row.size() - i - 1] ^ 1;
-                row[row.size() - i - 1] = temp;
+            int rowLength = row.size(); 
+            int left = 0;
+            int right = rowLength - 1;
+            while (left <= right) {
+                int temp = row[left] ^ 1;
+                row[left] = row[right] ^ 1;
+                row[right] = temp;
+                left++;
+                right--;
             }
         }
         return image;
