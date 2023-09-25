44273bcb7f80c39f365f9a33537d6264b9ba6fa9
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-09-25 14:04:15 +0530

    Time: 0 ms (100%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,3 +1,4 @@
+/* xor method:->
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -9,5 +10,21 @@ public:
             res^=c;
         }
         return res;
+    }
+};
+*/ 
+// sum of two method:-> 
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sumOfS=0, sumOfT=0;
+        for(char c:s){
+            sumOfS+=c;
+        }
+        for(char c:t){
+            sumOfT+=c;
+        }
+        return static_cast<char>(sumOfT-sumOfS); // ascii to char 
     }
 };
