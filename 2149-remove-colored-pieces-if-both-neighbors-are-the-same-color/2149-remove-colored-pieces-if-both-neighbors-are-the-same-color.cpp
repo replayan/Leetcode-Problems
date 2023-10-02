9b5da4b8b90e457b25107b308774e13bbff3712d
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-10-02 13:37:08 +0530

    Time: 38 ms (24.97%), Space: 13.6 MB (17.42%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -3,7 +3,7 @@ public:
     bool winnerOfGame(string colors) {
         int ca=0, cb=0;
         for(int i=0; i<colors.length(); ++i){
-            if(colors[i+1]==colors[i] && colors[i]==colors[i+2]){
+            if(colors[i]==colors[i+1] && colors[i]==colors[i+2]){ // checking adjacent chars
                 if(colors[i]=='A'){
                     ca++;
                 } else{
