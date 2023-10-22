caa2b53be815b4646760ced9ea3c10d5cd826968
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-10-22 12:21:25 +0530

    Time: 0 ms (100%), Space: 8.1 MB (8.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string removeSentence;
-        for(char c:s){
+        for(char c:s){ // remove commas,or any expressions like :,'
             if(isalnum(c)){
                 removeSentence+=c;
             }
         }
-        for(char &c:removeSentence){
+        for(char &c:removeSentence){ // convert it to lowercase to avoid malfunction
             c=tolower(c);
         }
         int left=0;
         int right=removeSentence.size()-1;
-        while(left<right){
+        while(left<right){ // used two pointer technique to check first and last and then move
             if(removeSentence[left]!=removeSentence[right]){
                 return false;
             }
