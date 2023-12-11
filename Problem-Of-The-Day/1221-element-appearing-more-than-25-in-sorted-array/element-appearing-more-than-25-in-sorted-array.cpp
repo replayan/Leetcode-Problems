53ca53b2f04899f5cfeea1beb6050160413cfd7e
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-12-11 13:02:13 +0530

    Time: 13 ms (42.19%) | Memory: 12.8 MB (57.06%) - LeetSync

---
+++ Problem-Of-The-Day/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findSpecialInteger(std::vector<int>& arr) {
+        int n=arr.size();
+        int threshold=n/4;
+        for (int i=0; i<n-threshold; ++i) {
+            if (arr[i]==arr[i+threshold]) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
