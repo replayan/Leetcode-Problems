5157b9bf6f21316c9d4bed0f1cc7a6bd0de58b88
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2023-12-16 12:35:01 +0530

    Time: 3 ms (97.78%) | Memory: 7.7 MB (79.21%) - LeetSync

---
+++ Problem-Of-The-Day/242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        vector<int> charCount(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            charCount[t[i] - 'a']--;
+        }
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
