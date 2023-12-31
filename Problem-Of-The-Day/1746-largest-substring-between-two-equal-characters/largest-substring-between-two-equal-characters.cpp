b1cf722632fedbf497b2bde67990be0e36f1be02
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-01-01 00:29:04 +0530

    Time: 4 ms (16.71%) | Memory: 6.6 MB (75.31%) - LeetSync

--- Problem-Of-The-Day/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ Problem-Of-The-Day/1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        unordered_map<char, int> firstOccurrence;
         int maxLength = -1;
         for (int i = 0; i < s.size(); ++i) {
-            char currentChar = s[i];
-            if (firstOccurrence.find(currentChar) == firstOccurrence.end()) {
-                // If the character is not in the map, store its first occurrence
-                firstOccurrence[currentChar] = i;
-            } else {
-                // If the character is in the map, calculate the length and update maxLength
-                int currentLength = i - firstOccurrence[currentChar] - 1;
-                maxLength = max(maxLength, currentLength);
+            for (int j = i + 1; j < s.size(); ++j) {
+                if (s[i] == s[j]) {
+                    maxLength = max(maxLength, j - i - 1);
+                }
             }
         }
         return maxLength;
