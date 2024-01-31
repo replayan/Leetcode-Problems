1c6db6218ae4374627f13301c3c740cc41da5aa2
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-02-01 00:43:32 +0530

    Time: 121 ms (89.61%) | Memory: 101 MB (67.32%) - LeetSync

--- Problem-Of-The-Day/739-daily-temperatures/daily-temperatures.cpp
+++ Problem-Of-The-Day/739-daily-temperatures/daily-temperatures.cpp
@@ -5,7 +5,7 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n=temperatures.size();
         stack<int> st;
-        vector<int> res(n,0);
+        vector<int> res(n);
         for(int i=n-1; i>=0; --i){
             while(!st.empty() && temperatures[i]>=temperatures[st.top()]){
                 st.pop();
