8cdf341497df09f1ac2d5a6ced6fc4bdc050a84c
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-02-08 01:01:04 +0530

    Time: 10 ms (69.23%) | Memory: 9.7 MB (37.02%) - LeetSync

--- Problem-Of-The-Day/451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ Problem-Of-The-Day/451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> freqMap;
@@ -15,3 +15,38 @@ public:
         return s;
     }
 };
+*/
+class Solution {
+public:
+    string frequencySort(string s) {
+        string sortedString;
+        int frequency[256] = {0}; 
+        for (char c : s) {
+            frequency[c]++;
+        }
+        while (true) {
+            char maxChar = '\0';
+            int maxFreq = 0;
+            
+            // Find the character with the maximum frequency
+            for (int i = 0; i < 256; ++i) {
+                if (frequency[i] > maxFreq) {
+                    maxChar = (char)i;
+                    maxFreq = frequency[i];
+                }
+            }
+            // If max frequency is 0, all characters are sorted
+            if (maxFreq == 0) {
+                break;
+            }
+            // Append the character to the sorted string maxFreq times
+            for (int i = 0; i < maxFreq; ++i) {
+                sortedString.push_back(maxChar);
+            }
+            // Set the frequency of the character to 0 to mark it as processed
+            frequency[maxChar] = 0;
+        }
+        return sortedString;
+    }
+};
+
