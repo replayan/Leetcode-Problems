df7447586bb08a537ce853adbc675bc09b896d8c
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-02-17 14:06:58 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        string ss=to_string(N);
+        int count=0;
+        for(char c:ss){
+            int lD=c-'0'; // char to int
+            if(lD!=0 && N%lD==0){
+                count+=1;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
