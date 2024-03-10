2ff23d71c5e15e2e9841874f808d552631ab9a8a
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-10 13:15:42 +0530

    Time: 3 ms (83.79%) | Memory: 13.4 MB (15.40%) - LeetSync

--- Problem-Of-The-Day/349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ Problem-Of-The-Day/349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -4,14 +4,24 @@ public:
 
         // (A ∩ B) is the set of all the elements that are common to both sets A
         // and B.
+        // unordered_set<int> st1(nums1.begin(), nums1.end());
+        // unordered_set<int> st2(nums2.begin(), nums2.end());
+        // vector<int> res;
+        // for (auto const& num : st1) {
+        //     if (st2.find(num) != st2.end()) {
+        //         res.push_back(num);
+        //     }
+        // }
+        // return res;
+
         unordered_set<int> st1(nums1.begin(), nums1.end());
-        unordered_set<int> st2(nums2.begin(), nums2.end());
-        vector<int> res;
-        for (auto const& num : st1) {
-            if (st2.find(num) != st2.end()) {
-                res.push_back(num);
+        unordered_set<int> st2;
+        for (auto const& num : nums2) {
+            if (st1.find(num) != st1.end()) {
+                st2.insert(num);
             }
         }
+        vector<int> res(st2.begin(), st2.end());
         return res;
     }
 };
