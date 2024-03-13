9d2ee6933318397958000417b2de1064d62d3306
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-14 01:00:46 +0530

    Time: 4 ms (35.26%) | Memory: 7.2 MB (41.86%) - LeetSync

--- Problem-Of-The-Day/2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ Problem-Of-The-Day/2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -2,6 +2,7 @@
 class Solution {
 public:
     int pivotInteger(int n) {
+        /*
         for (int p = 1; p <= n; ++p) {
             int lSum = 0;
             int rSum = 0;
@@ -16,5 +17,20 @@ public:
             }
         }
         return -1;
+        */
+        // o(n) solution
+        int lSum = 1;
+        int rSum = n;
+        int i = 1, j = n;
+        while (i < j) {
+            if (lSum < rSum) {
+                ++i;
+                lSum += i;
+            } else {
+                --j;
+                rSum += j;
+            }
+        }
+        return lSum == rSum ? i : -1;
     }
 };
