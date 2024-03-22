93b2a2b5898aba62fa667be1a9714cce6b44faf4
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-22 17:50:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if Linked List is Palindrome/check-if-linked-list-is-palindrome.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+#include <bits/stdc++.h>
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        //Your code here
+        vector<int> temp;
+        Node* current = head;
+        while (current != NULL) {
+            temp.push_back(current->data);
+            current = current->next;
+        }
+        for (int i = 0, j = temp.size() - 1; i < j; i++, --j) {
+            if (temp[i] != temp[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
