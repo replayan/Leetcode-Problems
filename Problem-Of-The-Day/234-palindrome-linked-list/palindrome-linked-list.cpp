1c688025825d0d634d4c52166f886cfd370931f8
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-22 17:05:12 +0530

    Time: 180 ms (41.17%) | Memory: 130.9 MB (13.75%) - LeetSync

--- Problem-Of-The-Day/234-palindrome-linked-list/palindrome-linked-list.cpp
+++ Problem-Of-The-Day/234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -7,13 +7,10 @@ public:
             temp.push_back(current->val);
             current = current->next;
         }
-        int i = 0, j = temp.size() - 1;
-        while (i < j) {
+        for (int i = 0, j = temp.size() - 1; i < j; i++, --j) {
             if (temp[i] != temp[j]) {
                 return false;
             }
-            ++i;
-            --j;
         }
         return true;
     }
