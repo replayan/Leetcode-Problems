735a53c17eddd1a20f44af4b893da18bbbdde4f1
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-24 14:03:40 +0530

    Time: 79 ms (63.15%) | Memory: 63.6 MB (77.16%) - LeetSync

--- Problem-Of-The-Day/287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ Problem-Of-The-Day/287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -19,7 +19,9 @@ public:
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
+
         slow = nums[0];
+
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
