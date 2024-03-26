0cdc41db1e4bfb94fc736e58eb3b29684b67dced
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-26 12:48:00 +0530

    Time: 38 ms (83.67%) | Memory: 43.7 MB (55.21%) - LeetSync

---
+++ Problem-Of-The-Day/41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,21 @@
+// after 10 trys : cycle sort algorithm TC O(n), SC O(1)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (int i = 0; i < nums.size();) {
+            long long correct_index = static_cast<long long>(nums[i]) - 1;
+            if (nums[i] > 0 && nums[i] <= nums.size() &&
+                nums[i] != nums[correct_index]) {
+                swap(nums[i], nums[correct_index]);
+            } else {
+                i++;
+            }
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.size() + 1;
+    }
+};
