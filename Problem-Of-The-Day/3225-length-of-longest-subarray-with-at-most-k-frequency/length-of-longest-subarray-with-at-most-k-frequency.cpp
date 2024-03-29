d18d39c312031f3219636dd268d23de86eed77d0
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-29 11:36:19 +0530

    Time: 294 ms (26.09%) | Memory: 289.7 MB (5.04%) - LeetSync

--- Problem-Of-The-Day/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ Problem-Of-The-Day/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,23 +4,23 @@ public:
         int ans = 0;
         unordered_map<int, queue<int>> first_occur;
         int n = nums.size();
-        for (int l = 0, r = 0; r < n; r++) {
+        for (int i = 0, j = 0; j < n; ++j) {
             // Push the current index into the queue for the current element
-            first_occur[nums[r]].push(r);
+            first_occur[nums[j]].push(j);
 
             // If the frequency of the current element is k+1
             // (i.e., the size of the queue is k+1)
-            if (first_occur[nums[r]].size() == k + 1) {
+            if (first_occur[nums[j]].size() == k + 1) {
                 // If the first occurrence of the current element is within the current window
-                if (first_occur[nums[r]].front() >= l) {
+                if (first_occur[nums[j]].front() >= i) {
                     // Update the left pointer to start from the next index
-                    l = first_occur[nums[r]].front() + 1;
+                    i = first_occur[nums[j]].front() + 1;
                 }
                 // Remove the oldest occurrence of the current element from the queue
-                first_occur[nums[r]].pop();
+                first_occur[nums[j]].pop();
             }
             // Update the maximum length of subarray with sum k
-            ans = max(ans, r - l + 1);
+            ans = max(ans, j - i + 1);
         }
         // Returning the maximum length of subarray with sum k
         return ans;
