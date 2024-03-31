23011066611ef4f8aa8a28c1f26eb10a50c2e708
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-03-31 14:38:46 +0530

    Time: 88 ms (62.77%) | Memory: 82.5 MB (53.99%) - LeetSync

--- SLIDING-WINDOW-TECHNIQUE/2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ SLIDING-WINDOW-TECHNIQUE/2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,22 +1,24 @@
+// Updated variable names for better understanding...
+
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
         long long count = 0;
-        int prev_minimum = -1;
-        int prev_maximum = -1;
-        int startAgain = -1;
+        int lastMinIndex = -1;
+        int lastMaxIndex = -1;
+        int resetIndex = -1;
         for (int left = 0; left < nums.size(); ++left) {
             if (nums[left] < minK || nums[left] > maxK) {
-                prev_minimum = prev_maximum = -1;
-                startAgain = left;
+                lastMinIndex = lastMaxIndex = -1;
+                resetIndex = left;
             }
             if (nums[left] == minK) {
-                prev_minimum = left;
+                lastMinIndex = left;
             }
             if (nums[left] == maxK) {
-                prev_maximum = left;
+                lastMaxIndex = left;
             }
-            count += max(0, min(prev_minimum, prev_maximum) - startAgain);
+            count += max(0, min(lastMinIndex, lastMaxIndex) - resetIndex);
         }
         return count;
     }
