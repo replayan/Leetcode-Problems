a112ce55ac9d2cd640bb121cabb1caccd87926d7
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-04-01 17:06:29 +0530

    Time: 4 ms (47.87%) | Memory: 7.8 MB (7.54%) - LeetSync

--- Problem-Of-The-Day/58-length-of-last-word/length-of-last-word.cpp
+++ Problem-Of-The-Day/58-length-of-last-word/length-of-last-word.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
+        int length_of_last_word = 0;
         for (int i = s.size() - 1; i >= 0; --i) {
             if (s[i] != ' ') {
-                length++;
-            } else if (length > 0) {
+                ++length_of_last_word;
+            } else if (length_of_last_word > 0) {
                 break;
             }
         }
-        return length;
+        return length_of_last_word;
     }
 };
