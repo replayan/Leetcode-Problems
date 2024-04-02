d2fc0ecf6838576efec31257675f4e97f5178269
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-04-02 13:05:26 +0530

    Time: 6 ms (65.28%) | Memory: 8.1 MB (85.95%) - LeetSync

--- STRINGS/205-isomorphic-strings/isomorphic-strings.cpp
+++ STRINGS/205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> mp1(256, -1); // Mapping from characters in s to characters in t
-        vector<int> mp2(256, -1); // Mapping from characters in t to characters in s
 
-        for (int i = 0; i < s.size(); ++i) {
-            int c1 = s[i]; 
-            int c2 = t[i];
+        if (s.length() == t.length()) {
+            vector<int> mp1(256, -1);
+            vector<int> mp2(256, -1);
+            for (int i = 0; i < s.size(); ++i) {
+                int c1 = s[i];
+                int c2 = t[i];
 
-            // If neither character is mapped yet
-            if (mp1[c1] == -1 && mp2[c2] == -1) {
-                mp1[c1] = c2; // Map c1 to c2 in mp1
-                mp2[c2] = c1; // Map c2 to c1 in mp2
-            }
-            // If the mapping is invalid
-            else if (mp1[c1] != c2 || mp2[c2] != c1) {
-                return false; // Strings are not isomorphic
+                // If neither character is mapped yet
+                if (mp1[c1] == -1 && mp2[c2] == -1) {
+                    mp1[c1] = c2; // Map c1 to c2 in mp1
+                    mp2[c2] = c1; // Map c2 to c1 in mp2
+                }
+                // If the mapping is invalid
+                else if (mp1[c1] != c2 || mp2[c2] != c1) {
+                    return false; // Strings are not isomorphic
+                }
             }
         }
         return true; // Strings are isomorphic
