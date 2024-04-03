ffa5ee06896d6fa9b2e9d10259db1bbc4ed6306d
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-04-03 12:50:39 +0530

    Time: 206 ms (85.79%) | Memory: 9.5 MB (96.96%) - LeetSync

--- STRINGS/79-word-search/word-search.cpp
+++ STRINGS/79-word-search/word-search.cpp
@@ -1,76 +1,45 @@
-class TrieNode {
-public:
-    unordered_map<char, TrieNode*> children;
-    bool isEndOfWord;
-
-    TrieNode() {
-        isEndOfWord = false;
-    }
-};
-
-class Trie {
-public:
-    TrieNode* root;
-
-    Trie() {
-        root = new TrieNode();
-    }
-
-    void insert(string word) {
-        TrieNode* node = root;
-        for (char c : word) {
-            if (!node->children.count(c)) {
-                node->children[c] = new TrieNode();
-            }
-            node = node->children[c];
-        }
-        node->isEndOfWord = true;
-    }
-};
-
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        Trie trie;
-        trie.insert(word);
+        int m = board.size(); // Get the number of rows in the board
+        int n = board[0].size(); // Get the number of columns in the board
 
-        int m = board.size();
-        int n = board[0].size();
-
+        // Iterate over each cell in the board
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (dfs(board, trie.root, i, j)) {
-                    return true;
+                // Call the dfs function starting from the current cell
+                if (dfs(board, word, 0, i, j)) {
+                    return true; // Return true if the word is found
                 }
             }
         }
 
-        return false;
+        return false; // Return false if the word is not found
     }
 
 private:
-    bool dfs(vector<vector<char>>& board, TrieNode* node, int i, int j) {
-        if (node->isEndOfWord) {
+    bool dfs(vector<vector<char>>& board, string& word, int idx, int i, int j) {
+        // Base case: If idx is equal to the length of the word, it means we have matched all characters
+        if (idx == word.length()) {
             return true;
         }
 
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size()) {
+        // Check if the current cell is out of bounds or if the character in the cell doesn't match the current character in the word
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx]) {
             return false;
         }
 
-        char c = board[i][j];
-        if (c == '#' || !node->children.count(c)) {
-            return false;
-        }
-
+        // Mark the current cell as visited by changing its value to a special character (e.g., '#')
         board[i][j] = '#';
 
-        bool found = dfs(board, node->children[c], i + 1, j) ||
-                     dfs(board, node->children[c], i - 1, j) ||
-                     dfs(board, node->children[c], i, j + 1) ||
-                     dfs(board, node->children[c], i, j - 1);
+        // Recursively explore the neighboring cells (up, down, left, right)
+        bool found = dfs(board, word, idx + 1, i + 1, j) || // Explore the cell below
+                     dfs(board, word, idx + 1, i - 1, j) || // Explore the cell above
+                     dfs(board, word, idx + 1, i, j + 1) || // Explore the cell to the right
+                     dfs(board, word, idx + 1, i, j - 1);   // Explore the cell to the left
 
-        board[i][j] = c;
+        // Restore the original character in the current cell (backtrack)
+        board[i][j] = word[idx];
 
         return found;
     }
