66630e675a3e1d1a47d2d1c703fdd4b8907bbc03
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-04-04 13:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (49.50%) - LeetSync

--- STRINGS/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ STRINGS/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        /* without using stack DS SC => O(1)
         int counter = 0, maxCounter = 0;
         for (int i = 0; i < s.size(); ++i) {
             if (s[i] == '(') {
@@ -9,6 +10,21 @@ public:
                 --counter;
             }
             maxCounter = max(maxCounter, counter);
+        }
+        return maxCounter;
+        */
+
+        // using stack DS SC => O(N)
+
+        stack<int> st;
+        int maxCounter = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                st.pop();
+            }
+            maxCounter = max(maxCounter, (int)st.size());
         }
         return maxCounter;
     }
