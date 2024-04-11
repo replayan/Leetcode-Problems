270d02ed7218bba0e84a87bd1c42bce252171268
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-04-11 12:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (67.60%) - LeetSync

--- QUEUE-SIMULATION/987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ QUEUE-SIMULATION/987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -2,20 +2,30 @@ class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         sort(deck.begin(), deck.end());
-        const int n = deck.size();
-        queue<int> q;
+        int n = deck.size();
+        vector<int> res(n);
+        deque<int> q;
+        
+        // Initialize the deque with indices
         for (int i = 0; i < n; ++i) {
-            q.push(i);
+            q.push_back(i);
         }
-        vector<int> r(n);
-        for (int i = 0, j = 0; i < n; j ^= 1) {
-            if (j) {
-                q.push(q.front());
-           } else {
-            r[q.front()] = deck[i++];
-           }
-           q.pop();
+        
+        for (int card : deck) {
+            // Get the index from the front of the deque
+            int idx = q.front();
+            q.pop_front();
+            
+            // Assign the card to the index
+            res[idx] = card;
+            
+            // Move the last index to the front of the deque
+            if (!q.empty()) {
+                q.push_back(q.front());
+                q.pop_front();
+            }
         }
-        return r;
+        
+        return res;
     }
 };
