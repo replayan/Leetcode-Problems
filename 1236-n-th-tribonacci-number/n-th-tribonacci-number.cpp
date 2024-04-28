5ef8787695d3fa4c27b82ff4decdc7e7ae536791
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-04-28 17:22:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (27.23%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> tribo = {0, 1, 1};
-        for (int i = 3; i <= n; ++i) {
-            tribo.push_back(tribo[i - 1] + tribo[i - 2] + tribo[i - 3]);
+        vector<int> t = {0, 1, 1};
+        int i = 3;
+        
+        if (n < 3) {
+            goto base_case;
         }
-        return tribo[n];
+        
+        loop:
+        t.push_back(t[i - 1] + t[i - 2] + t[i - 3]);
+        i++;
+        if (i <= n) {
+            goto loop;
+        }
+        
+        base_case:
+        return t[n];
     }
 };
