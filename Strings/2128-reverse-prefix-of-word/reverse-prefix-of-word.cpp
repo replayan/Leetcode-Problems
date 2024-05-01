79daeea95f7f1ae9afa6e7df260c69e1cc357dfd
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-05-01 18:44:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (91.39%) - LeetSync

--- Strings/2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ Strings/2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,9 +1,25 @@
+/*
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
         const char* chr = strchr(word.c_str(), ch);
         if (chr) {
             reverse(word.begin(), word.begin() + (chr - word.c_str()) + 1);
+        }
+        return word;
+    }
+};
+*/
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int j=word.find(ch);
+        int i=0;
+        while(i<j){
+            swap(word[i],word[j]);
+            ++i;
+            --j;
         }
         return word;
     }
