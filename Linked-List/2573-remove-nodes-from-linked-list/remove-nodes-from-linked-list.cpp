7ada801f899ddba6c6929f1c3fb3c5c36b5e84ea
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-05-06 18:17:36 +0530

    Time: 363 ms (5.00%) | Memory: 170.7 MB (29.07%) - LeetSync

--- Linked-List/2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ Linked-List/2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
+        // If the linked list is empty or has only one node, return the head
         if (head == NULL || head->next == NULL) {
             return head;
         }
+
+        // Recursively remove nodes from the rest of the list
         ListNode* nextNode = removeNodes(head->next);
+
+        // If the current node's value is less than the next node's value
         if (head->val < nextNode->val) {
+            // Delete the current node
             delete head;
+            // Return the next node as the new head
             return nextNode;
         }
+
+        // Otherwise, keep the current node
+        // and update its next pointer to the modified list
         head->next = nextNode;
         return head;
     }
