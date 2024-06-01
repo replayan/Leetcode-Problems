f69e6a1a9bc7fcff61fafbfb79383257c40d8726
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-01 13:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.98%) - LeetSync

--- STRINGS/3379-score-of-a-string/score-of-a-string.cpp
+++ STRINGS/3379-score-of-a-string/score-of-a-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        // int n = size(s) - 1;
         int score = 0;
         for (int i = 0; s[i + 1] != '\0'; ++i) {
             score += abs(s[i] - s[i + 1]);
