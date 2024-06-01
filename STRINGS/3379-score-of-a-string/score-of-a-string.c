317a078997654c70c398ddf0b94cd572ac52945e
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-01 13:28:01 +0530

    Time: 0 ms (100.00%) | Memory: 5.4 MB (60.20%) - LeetSync

--- STRINGS/3379-score-of-a-string/score-of-a-string.c
+++ STRINGS/3379-score-of-a-string/score-of-a-string.c
@@ -1,12 +1,8 @@
-int scoreOfString(char *s) {
-    int n = strlen(s) - 1;
+int scoreOfString(const char *s) {
     int score = 0;
-    for (int i = 0; i < n; ++i) {
-        if (s[i] > s[i + 1]) {
-            score += s[i] - s[i + 1];
-        } else {
-            score += s[i + 1] - s[i];
-        }
+    for (int i = 0; s[i + 1] != '\0'; ++i) {
+        int diff = s[i] - s[i + 1];
+        score += diff > 0 ? diff : -diff;
     }
     return score;
 }
