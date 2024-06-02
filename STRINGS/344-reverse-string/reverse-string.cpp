4a4da80954963785bdb9f0024438a91da05d18e4
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-02 17:10:19 +0530

    Time: 27 ms (5.65%) | Memory: 27 MB (21.89%) - LeetSync

--- STRINGS/344-reverse-string/reverse-string.cpp
+++ STRINGS/344-reverse-string/reverse-string.cpp
@@ -3,12 +3,12 @@ public:
     void reverseString(vector<char>& s) {
         int left = 0;
         int right = size(s) - 1;
-        while (left < right) {
+        for (; left <= right;) {
             if (s[left] != s[right]) {
                 swap(s[left], s[right]);
             }
-            left += 1;
-            right -= 1;
+            left++;
+            right--;
         }
     }
 };
