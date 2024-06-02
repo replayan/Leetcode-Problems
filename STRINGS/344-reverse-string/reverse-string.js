02afe6b5065e60e04257faace89fd0526f364433
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-02 12:15:22 +0530

    Time: 92 ms (7.68%) | Memory: 57.2 MB (44.13%) - LeetSync

--- STRINGS/344-reverse-string/reverse-string.js
+++ STRINGS/344-reverse-string/reverse-string.js
@@ -1,6 +1,6 @@
 var reverseString = function (s) {
-    left = 0
-    right = s.length - 1
+    let left = 0
+    let right = s.length - 1
     while (left < right) {
         if (s[left] !== s[right]) {
             [s[left], s[right]] = [s[right], s[left]];
