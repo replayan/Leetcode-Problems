b051b498a1ce899cbd7fe92e82db86b52248a9a2
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-05 14:44:52 +0530

    Time: 55 ms (28.97%) | Memory: 16.6 MB (67.21%) - LeetSync

---
+++ STRINGS/1044-find-common-characters/find-common-characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        cnt = Counter(words[0])
+        for w in words:
+            curr_cnt = Counter(w)
+            for c in cnt:
+                cnt[c] = min(cnt[c], curr_cnt[c])
+
+        res = []
+        for c in cnt:
+            for i in range(cnt[c]):
+                res.append(c)
+        return res
