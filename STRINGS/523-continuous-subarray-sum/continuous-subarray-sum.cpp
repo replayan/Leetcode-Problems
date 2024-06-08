baab21c7043ccafc5cba27a92b3b6cf50f74253a
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-09 01:13:53 +0530

    Time: 183 ms (47.42%) | Memory: 134.7 MB (59.63%) - LeetSync

--- STRINGS/523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ STRINGS/523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_set<int> have;
+        unordered_set<int> st;
         for (int p = 0, i = 0; i < nums.size(); ++i) {
             const int temp = (p + nums[i]) % k;
-            if (have.count(temp)) {
+            if (st.count(temp)) {
                 return true;
             }
-            have.insert(p);
+            st.insert(p);
             p = temp;
         }
         return false;
