6fbfa97a35c36da38e0273ec72a9a99ea7cdab06
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-13 15:49:10 +0530

    Time: 7 ms (60.18%) | Memory: 21 MB (17.35%) - LeetSync

--- Prefix-sum-SUBARRAY/2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ Prefix-sum-SUBARRAY/2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(), seats.end());
-        sort(students.begin(), students.end());
-        
-        int moves = 0;
-        for (auto i = 0; i < seats.size(); ++i) {
+        sort(begin(seats), end(seats));
+        sort(begin(students), end(students));
+
+        int moves = 0, n = size(seats);
+        for (int i = 0; i < n; ++i) {
             moves += abs(seats[i] - students[i]);
         }
         return moves;
