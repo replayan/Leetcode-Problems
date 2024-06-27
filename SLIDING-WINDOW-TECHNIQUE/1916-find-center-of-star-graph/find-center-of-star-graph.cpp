1616f1225f2d4ae287b8337e06fca1c0323c7c93
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-06-27 22:50:44 +0530

    Time: 136 ms (61.47%) | Memory: 70.6 MB (98.95%) - LeetSync

--- SLIDING-WINDOW-TECHNIQUE/1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ SLIDING-WINDOW-TECHNIQUE/1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
+        /* o(n) TC and o(n) SC
         unordered_map<int, int> degree;
         for (auto& vec : edges) {
             int u = vec[0];
@@ -14,5 +15,13 @@ public:
             }
         }
         return -1;
+        */
+
+        vector<int> first = edges[0];
+        vector<int> second = edges[1];
+        if (first[0] == second[0] || first[0] == second[1]) {
+            return first[0];
+        }
+        return first[1];
     }
 };
