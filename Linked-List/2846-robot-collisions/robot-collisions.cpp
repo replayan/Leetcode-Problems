f6159f32edbcc06721b25bb797c27a0ff4f073a5
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-07-13 17:53:17 +0530

    Time: 199 ms (82.44%) | Memory: 195.2 MB (87.79%) - LeetSync

--- Linked-List/2846-robot-collisions/robot-collisions.cpp
+++ Linked-List/2846-robot-collisions/robot-collisions.cpp
@@ -1,66 +1,44 @@
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        int n = positions.size();
-        vector<int> result;
-        
-        // Create a vector of tuples to hold the combined data along with original indices
-        vector<tuple<int, int, char, int>> robots;
+    vector<int> survivedRobotsHealths(vector<int>& positions,
+                                      vector<int>& healths, string directions) {
+        const int n = positions.size();
+        vector<int> ind(n);
         for (int i = 0; i < n; ++i) {
-            robots.emplace_back(positions[i], healths[i], directions[i], i);
+            ind[i] = i;
         }
-
-        // Sort robots by positions
-        sort(robots.begin(), robots.end());
-
-        // Use a stack to handle the collisions
-        stack<tuple<int, int, char, int>> st;
-
-        for (auto& robot : robots) {
-            int pos = get<0>(robot);
-            int health = get<1>(robot);
-            char direction = get<2>(robot);
-            int idx = get<3>(robot);
-
-            if (direction == 'R') {
-                st.push(robot);
-            } else { // direction == 'L'
-                while (!st.empty() && get<2>(st.top()) == 'R') {
-                    auto [r_pos, r_health, r_direction, r_idx] = st.top();
-                    st.pop();
-
-                    if (r_health > health) {
-                        st.push({r_pos, r_health - 1, r_direction, r_idx});
-                        health = 0;
-                        break;
-                    } else if (r_health < health) {
-                        health -= 1;
+        sort(ind.begin(), ind.end(), [&](const int x, const int y) {
+            return positions[x] < positions[y];
+        });
+        stack<int> s;
+        for (int x : ind) {
+            if (directions[x] == 'L') {
+                while (!s.empty() && healths[x]) {
+                    const int y = s.top();
+                    if (healths[x] == healths[y]) {
+                        healths[x] = healths[y] = 0;
+                        s.pop();
+                    } else if (healths[x] > healths[y]) {
+                        --healths[x];
+                        healths[y] = 0;
+                        s.pop();
                     } else {
-                        health = 0;
-                        break;
+                        healths[x] = 0;
+                        if (--healths[y] == 0) {
+                            s.pop();
+                        }
                     }
                 }
-                if (health > 0) {
-                    st.push({pos, health, direction, idx});
-                }
+            } else {
+                s.push(x);
             }
         }
-
-        // Collect the healths of surviving robots in their original order
-        vector<pair<int, int>> survivors;
-        while (!st.empty()) {
-            auto [pos, health, direction, idx] = st.top();
-            st.pop();
-            survivors.push_back({idx, health});
+        vector<int> r;
+        for (int x : healths) {
+            if (x) {
+                r.push_back(x);
+            }
         }
-        
-        // Sort survivors by their original indices
-        sort(survivors.begin(), survivors.end());
-
-        for (auto& survivor : survivors) {
-            result.push_back(survivor.second);
-        }
-
-        return result;
+        return r;
     }
 };
