f46ea04659398ae2352b1ee3eb7e9c9bf1e7159e
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-07-24 17:16:27 +0530

    Time: 933 ms (82.06%) | Memory: 24.7 MB (60.87%) - LeetSync

--- TREES/Matrix/1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ TREES/Matrix/1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,12 +1,12 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        pair=[]
-        for i,n in  enumerate(nums):
-            n=str(n)
-            mapped_n=0
+        pair = []
+        for i, n in enumerate(nums):
+            n = str(n)
+            mapped_n = 0
             for c in n:
-                mapped_n*=10
-                mapped_n+=mapping[int(c)]
-            pair.append((mapped_n,i))
+                mapped_n *= 10
+                mapped_n += mapping[int(c)]
+            pair.append((mapped_n, i))
         pair.sort()
         return [nums[p[1]] for p in pair]
