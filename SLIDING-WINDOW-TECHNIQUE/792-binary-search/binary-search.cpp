17bc14263cee0c74d1779a69b3c98797c0dafa29
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-08-13 21:31:35 +0530

    Time: 14 ms (98.88%) | Memory: 30.3 MB (13.96%) - LeetSync

--- SLIDING-WINDOW-TECHNIQUE/792-binary-search/binary-search.cpp
+++ SLIDING-WINDOW-TECHNIQUE/792-binary-search/binary-search.cpp
@@ -3,7 +3,7 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0, end = size(nums) - 1, mid;
         while (start <= end) {
-            mid = start + (end - start) / 2;
+            mid = (start + end) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
