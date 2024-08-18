cf6bbf7e8ce3c4c6429a52c6f50711ea799353fd
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-08-18 14:38:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (76.75%) - LeetSync

---
+++ SLIDING-WINDOW-TECHNIQUE/264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> uglyNumbers(n);
+        uglyNumbers[0] = 1;  // The first ugly number is 1.
+        
+        int i2 = 0, i3 = 0, i5 = 0;  // Indices for multiples of 2, 3, 5.
+        int nextMultipleOf2 = 2;
+        int nextMultipleOf3 = 3;
+        int nextMultipleOf5 = 5;
+        int nextUglyNumber = 1;
+
+        for (int i = 1; i < n; i++) {
+            // Choose the smallest number as the next ugly number.
+            nextUglyNumber = min({nextMultipleOf2, nextMultipleOf3, nextMultipleOf5});
+            uglyNumbers[i] = nextUglyNumber;
+            
+            // Update the pointer for the chosen number.
+            if (nextUglyNumber == nextMultipleOf2) {
+                i2++;
+                nextMultipleOf2 = uglyNumbers[i2] * 2;
+            }
+            if (nextUglyNumber == nextMultipleOf3) {
+                i3++;
+                nextMultipleOf3 = uglyNumbers[i3] * 3;
+            }
+            if (nextUglyNumber == nextMultipleOf5) {
+                i5++;
+                nextMultipleOf5 = uglyNumbers[i5] * 5;
+            }
+        }
+
+        return uglyNumbers[n - 1];
+    }
+};
