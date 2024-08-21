6fb23ab3e14b286745e321edb0f92caaf6a4b551
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-08-22 00:12:20 +0530

    Time: 14 ms (79.88%) | Memory: 11.1 MB (47.31%) - LeetSync

---
+++ nth-number-problems/664-strange-printer/strange-printer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int len = 1; len <= n; ++len) {
+            for (int i = 0; i + len - 1 < n; ++i) {
+                int j = i + len - 1;
+                dp[i][j] = 1 + ((j == i) ? 0 : dp[i + 1][j]);
+                for (int k = i + 1; k <= j; ++k) {
+                    if (s[i] == s[k]) {
+                        dp[i][j] = min(dp[i][j], ((k - 1 >= i) ? dp[i][k - 1] : 0) + ((k + 1 <= j) ? dp[k + 1][j] : 0));
+                    }
+                }
+            }
+               
+        }
+                
+        return dp[0][n - 1];
+        
+    }
+};
