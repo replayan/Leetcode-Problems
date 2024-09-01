9f94dfd8fdc5a2c0381eea3020ff03b2b9f3e4b8
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2024-09-01 19:04:42 +0530

    Time: 80 ms (65.77%) | Memory: 92.8 MB (25.95%) - LeetSync

--- nth-number-problems/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ nth-number-problems/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -5,12 +5,13 @@ public:
         int k = size(original);
         vector<vector<int>> mat(m, vector<int>(n));
         if (m * n != k) {
-            return {};
+            return {}; // if Inconsistent Lengths return empty array
         }
         int idx = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                mat[i][j] = original[idx++];
+                mat[i][j] = original[idx];
+                idx += 1;
             }
         }
         return mat;
