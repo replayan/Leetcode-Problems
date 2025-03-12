d75984abd3ff658faa5f6fc8f36c45cb5bfc5f47
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2025-03-12 12:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (75.85%) - LeetSync

--- Linked-List/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ Linked-List/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        long long positive_count = 0, negative_count = 0;
-        for (int i = 0; i < size(nums); ++i) {
-            if (nums[i] > 0) {
-                ++positive_count;
-            } else if (nums[i] < 0) {
-                ++negative_count;
+        int negative_count = 0, positive_count = 0;
+        int left = 0, right = nums.size() - 1;
+        
+        // Find rightmost negative
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < 0) {
+                negative_count = mid + 1;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+        
+        left = 0, right = nums.size() - 1;
+        
+        // Find leftmost positive
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > 0) {
+                positive_count = nums.size() - mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
         return max(positive_count, negative_count);
     }
 };
