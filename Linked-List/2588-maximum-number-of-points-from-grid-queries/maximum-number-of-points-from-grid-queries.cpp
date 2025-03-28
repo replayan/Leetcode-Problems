51864d2fc2baf60b796c5176cbc7fd8d16184b98
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2025-03-28 13:18:20 +0530

    Time: 117 ms (47.10%) | Memory: 45 MB (42.75%) - LeetSync

--- Linked-List/2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ Linked-List/2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -3,28 +3,35 @@ public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
         int m = grid.size();
         int n = grid[0].size();
-        
+
+        // Edge case: if grid is empty
+        if (m == 0 || n == 0)
+            return vector<int>(queries.size(), 0);
+
         vector<vector<int>> min_max(m, vector<int>(n, INT_MAX));
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<>>
+            pq;
+
+        // Start from (0,0)
         min_max[0][0] = grid[0][0];
         pq.emplace(grid[0][0], 0, 0);
-        
-        vector<int> dirs = {-1, 0, 1, 0, -1}; // Directions: up, right, down, left
-        
+
+        vector<int> dirs = {-1, 0, 1, 0, -1}; // up, right, down, left
+
+        // Dijkstra to compute min of max values along paths
         while (!pq.empty()) {
             auto [current_max, i, j] = pq.top();
             pq.pop();
-            
-            // Skip if a better path was already found for this cell
+
             if (current_max > min_max[i][j]) {
                 continue;
             }
-            
+
             for (int d = 0; d < 4; ++d) {
                 int ni = i + dirs[d];
                 int nj = j + dirs[d + 1];
-                
+
                 if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
                     int new_max = max(current_max, grid[ni][nj]);
                     if (new_max < min_max[ni][nj]) {
@@ -34,8 +41,8 @@ public:
                 }
             }
         }
-        
-        // Collect all reachable cells' min_max values
+
+        // Collect reachable cells' min_max values
         vector<int> reachable_min_max;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -45,19 +52,16 @@ public:
             }
         }
         sort(reachable_min_max.begin(), reachable_min_max.end());
-        
+
         // Process each query
         vector<int> answer;
         for (int q : queries) {
-            if (q <= grid[0][0]) {
-                answer.push_back(0);
-            } else {
-                // Number of elements less than q is the upper_bound of q-1
-                auto it = upper_bound(reachable_min_max.begin(), reachable_min_max.end(), q - 1);
-                answer.push_back(it - reachable_min_max.begin());
-            }
+            // Count cells where min_max[i][j] < q
+            auto it = upper_bound(reachable_min_max.begin(),
+                                  reachable_min_max.end(), q - 1);
+            answer.push_back(it - reachable_min_max.begin());
         }
-        
+
         return answer;
     }
 };
