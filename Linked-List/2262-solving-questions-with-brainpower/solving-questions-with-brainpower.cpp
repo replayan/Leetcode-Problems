fd23fc68abe1b07cf3d1b5940f6f4084cec55b75
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2025-04-02 01:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 119.6 MB (95.05%) - LeetSync

--- Linked-List/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ Linked-List/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -5,7 +5,10 @@ public:
         vector<long long> dp(n);
         dp[n - 1] = questions[n - 1][0];
         for (int i = n - 2; i >= 0; --i) {
-            dp[i] = max(dp[i + 1], questions[i][0] + (i + questions[i][1] + 1 < n ? dp[i + questions[i][1] + 1] : 0));
+            dp[i] = max(dp[i + 1],
+                        questions[i][0] + (i + questions[i][1] + 1 < n
+                                               ? dp[i + questions[i][1] + 1]
+                                               : 0));
         }
         return dp[0];
     }
