491d7ecc5c853966609edadda1f7e317807e5700
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2025-04-07 13:36:04 +0530

    Time: 195 ms (12.90%) | Memory: 100.3 MB (45.21%) - LeetSync

--- Linked-List/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ Linked-List/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        if (totalSum % 2 != 0) {
-            return false;
-        }
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if (totalSum % 2 != 0) return false;
+
         int target = totalSum / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-        for (int num : nums) {
-            for (int j = target; j >= num; --j) {
-                if (dp[j - num]) {
-                    dp[j] = true;
-                }
-                if (dp[target]) {
-                    return true;
-                }
-            }
-        }
-        return dp[target];
+        int n = nums.size();
+
+        // -1: unvisited, 0: false, 1: true
+        vector<vector<int>> memo(n, vector<int>(target + 1, -1));
+
+        return dfs(nums, 0, 0, target, memo);
+    }
+
+    bool dfs(vector<int>& nums, int index, int currSum, int target, vector<vector<int>>& memo) {
+        if (currSum == target) return true;
+        if (index >= nums.size() || currSum > target) return false;
+
+        if (memo[index][currSum] != -1) return memo[index][currSum];
+
+        bool include = dfs(nums, index + 1, currSum + nums[index], target, memo);
+        bool exclude = dfs(nums, index + 1, currSum, target, memo);
+
+        memo[index][currSum] = include || exclude;
+        return memo[index][currSum];
     }
 };
