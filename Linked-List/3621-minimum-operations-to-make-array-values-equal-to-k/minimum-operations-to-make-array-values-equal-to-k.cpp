ae838dcc16110f06fb9b9643bdcfb23d744aef87
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2025-04-10 00:19:48 +0530

    Time: 15 ms (30.19%) | Memory: 32.5 MB (24.53%) - LeetSync

--- Linked-List/3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ Linked-List/3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,56 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+        unordered_set<int> distinctGreaterThanK;
         for (int num : nums) {
             if (num < k) {
-                return -1;
+                return -1; // Impossible if any element is less than k
+            }
+            if (num > k) {
+                distinctGreaterThanK.insert(num); // Collect distinct values > k
             }
         }
-
-        int operations = 0;
-
-        while (true) {
-            bool all_k = true;
-            for (int num : nums) {
-                if (num != k) {
-                    all_k = false;
-                    break;
-                }
-            }
-            if (all_k) {
-                return operations;
-            }
-
-            int max_val = nums[0];
-            for (int num : nums) {
-                if (num > max_val) {
-                    max_val = num;
-                }
-            }
-
-            int second_max = k;
-            for (int num : nums) {
-                if (num > second_max && num < max_val) {
-                    second_max = num;
-                }
-            }
-
-            int h = second_max;
-            for (int num : nums) {
-                if (num > h && num != max_val) {
-                    h = max_val - 1;
-                    break;
-                }
-            }
-
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[i] > h) {
-                    nums[i] = h;
-                }
-            }
-            operations++;
-        }
-
-        return operations;
+        return distinctGreaterThanK.size(); // Number of operations
     }
 };
