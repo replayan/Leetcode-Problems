f497b00a0226a6c10ece7fc1e08ba52fd012cfa8
Ayan Chakraborty <86905096+replayan@users.noreply.github.com>
2026-01-20 11:52:53 +0530

    Time: 3 ms (37.87%) | Memory: 25.2 MB (26.63%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -4,23 +4,16 @@ public:
         vector<int> ans;
         
         for (int p : nums) {
-            // x | (x + 1) is always odd → evens impossible
-            if ((p & 1) == 0) {
-                ans.push_back(-1);
-                continue;
+            int found = -1;
+            
+            for (int x = 0; x < p; x++) {
+                if ( (x | (x + 1)) == p ) {
+                    found = x;
+                    break;  // minimal by construction
+                }
             }
             
-            // Count trailing 1s
-            int t = 0;
-            int temp = p;
-            while (temp & 1) {
-                t++;
-                temp >>= 1;
-            }
-            
-            // Minimal x
-            int x = p - (1 << (t - 1));
-            ans.push_back(x);
+            ans.push_back(found);
         }
         
         return ans;
